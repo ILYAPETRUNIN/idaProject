d451d698d766fec005b12a4208128526c2904fe0
ILYA PETRUNIN <petrunin.cumbaa@gmail.com>
2022-08-02 20:42:02 +0500

    add btn

---
+++ components/BaseBtn.vue
@@ -0,0 +1,75 @@
+<template>
+  <button @click="click" :disabled="disabled" :class="getClasses" class="base-btn">
+    <slot />
+  </button>
+</template>
+
+<script>
+
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'success'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: Boolean
+    }
+  },
+  computed: {
+    getClasses () {
+      return [
+        this.icon ? 'icon' : '',
+        this.color ? this.color : '',
+        this.disabled ? 'disabled' : '',
+        this.text ? 'text' : ''
+      ]
+    }
+  },
+  methods: {
+    click () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.base-btn{
+  @include flexy(center,center);
+  @include setSize(100%,36px);
+  cursor:pointer;
+  border:none;
+  outline:none;
+  border-radius: 10px;
+}
+.primary{
+  background:scotch-color($theme-light,'primary')
+}
+.success{
+  background:scotch-color($theme-light,'success');
+  color:scotch-color($theme-light,'primary');
+  font-family: 'Inter';
+  font-weight: 600;
+  font-size: 12px;
+}
+.danger{
+  background:scotch-color($theme-light,'danger')
+}
+.disabled{
+  background: scotch-color($theme-light,'disabled');
+  color: scotch-color($theme-light,'text-light');
+  cursor:auto
+}
+.icon{
+  @include setSize(32px,32px);
+  padding: 8px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+}
+
+</style>
--- pages/UIkit.vue
+++ pages/UIkit.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="uikit">
-    <BaseInput name="Наименование товара" placeholder="Введите наименование товара" />
+    <BaseInput v-model="name" name="Наименование товара" placeholder="Введите наименование товара" />
     <BaseInput error="Ошибка" name="Наименование товара" placeholder="Введите наименование товара" required />
+    <BaseInput textarea name="Наименование товара" placeholder="Описание товара"/>
+    <BaseBtn>Добавить товар</BaseBtn>
+    <BaseBtn disabled>Добавить товар</BaseBtn>
+    <BaseBtn icon color='danger'>
+      <DeleteSvg />
+    </BaseBtn>
   </div>
 </template>
 
 <script>
+import DeleteSvg from '~/assets/icons/delete.svg?inline'
+
 export default {
+  components: { DeleteSvg },
+  data () {
+    return {
+      name: ''
+    }
+  }
 }
 </script>
 

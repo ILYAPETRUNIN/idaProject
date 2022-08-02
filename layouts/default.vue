b90850ab12116a5cd91180908f2e9a0308398c51
ILYA PETRUNIN <petrunin.cumbaa@gmail.com>
2022-08-02 11:28:13 +0500

    config project

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="layout-products">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">
+    Страница
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>

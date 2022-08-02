bd1b0b3b7394ee97e1836822433d1be7f9e43682
ILYA PETRUNIN <petrunin.cumbaa@gmail.com>
2022-08-02 22:58:17 +0500

    add dev tools

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,6 +43,12 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
+  vue:{
+    config:{
+      productionTip: false,
+      devtools: true
+    }
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
